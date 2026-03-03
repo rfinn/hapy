898ff97c82fd40fd1dc028f80a5ba180e841f160
rfinn <rfinn@siena.edu>
2026-03-03 12:44:34 -0500

    debugging run_analysis.py

--- hapy/ellipse/photometry.py
+++ hapy/ellipse/photometry.py
@@ -1222,7 +1222,7 @@ class EllipsePhotometry():
         '''
 
         # initialize conditions to implement a stopping radius
-        snr_stop = 3
+        snr_stop = 50
         snr_consecutive = 2
         low_snr_count = 0
 
