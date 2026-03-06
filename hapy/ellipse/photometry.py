- fixed naming issue with *phot.fits files - changing snr calculation in radial profiles to snr_per_pixel to determine cutoff

hapy/ellipse/photometry.py
@@ -1341,11 +1341,21 @@ class EllipsePhotometry():
                 #sigma_ann = self.sky_noise * np.sqrt(dA)
                 #
                 # Otherwise, reuse your existing noise model:
-                sigma_ann = self.get_noise_in_aper(dF, dA)
+                # what Becky did
+                # median sb in annulus as signal
+                # compared with noise_per_pixel = sqrt(sky_uncertainty**2 + median_sb)
+                # snr_per_pixel = median_sb/noise
 
-                snr_ann = dF / sigma_ann if (sigma_ann is not None and np.isfinite(sigma_ann) and sigma_ann > 0) else -np.inf
-                print(f"DEBUG: snr_ann={snr_ann:.1f}, dF={dF:.1f},sigma_ann={sigma_ann:.1e}, sky_noise={self.sky_noise:.1e}, dA={dA:.1f}")
+                ave_sb = dF/dA
+                noise_per_pixel = sqrt(self.sky_noise**2 + ave_sb) # does not account properly for gain...
+                snr_per_pixel = ave_sb/noise_per_pixel
+                
+                sigma_ann = self.get_noise_in_aper(dF, dA)
+                snr_ann =  dFdA / sigma_ann if (sigma_ann is not None and np.isfinite(sigma_ann) and sigma_ann > 0) else -np.inf
+                
+                print(f"DEBUG: snr_per_pixel={snr_per_pixel:.1f},snr_ann={snr_ann:.1f}, dF={dF:.1f},sigma_ann={sigma_ann:.1e}, sky_noise={self.sky_noise:.1e}, dA={dA:.1f}")
                 if snr_ann < snr_stop:
+                #if snr_per_pixel < snr_stop:                
                     low_snr_count += 1
                     if low_snr_count >= snr_consecutive:
                         # truncate arrays to i (inclusive) and stop

hapy/imagetools/plotting.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 from matplotlib import pyplot as plt
@@ -19,15 +20,24 @@ def ellipse_patch(xc, yc, sma_pix, ba, pa_deg, **kwargs):
     )
 
 
-def display_image(image, percent=99.9, lowrange=False, sigclip=True):
-    if sigclip:
+def display_image(image, percent=99.9, lowrange=False, sigclip=True,mask=None):
+    masked=False
+    if mask is not None:
+        #image = np.ma.array(image, mask=mask)
+        masked=True
+        statarray = image.copy()
+        statarray[mask] = np.nan
+    if sigclip and (mask is None):
         clipped_data = sigma_clip(image, sigma_lower=5, sigma_upper=5)
     else:
         clipped_data = image
 
     stretch = "linear" if lowrange else "asinh"
-    norm = simple_norm(clipped_data, stretch=stretch, percent=percent)
-    plt.imshow(image, norm=norm, cmap="gray_r", origin="lower")
+    if mask is not None:
+        norm = simple_norm(statarray, stretch=stretch, percent=percent)
+    else:
+        norm = simple_norm(clipped_data, stretch=stretch, percent=percent)
+    plt.imshow(image, norm=norm,origin="lower", cmap="gray_r")#, origin="lower")
 
 
 def display_unwise(ra,dec,galname,imsize_arcsec=60):
@@ -169,10 +179,16 @@ def plot_mask_ellipse_diagnostic(
 
     r_data, r_hdr = fits.getdata(r_fits, header=True)
     m_data = fits.getdata(mask_fits)
-
+    mmask = m_data > 0
+    rmasked = np.ma.array(r_data, mask=mmask)
+    #plt.figure()
+    #plt.imshow(rmasked, origin="lower")
+    #plt.savefig("debug-plot.png")
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     plt.sca(ax[0])
-    display_image(r_data)
+    display_image(r_data, mask=mmask)
+    #display_image(rmasked)
+    #plt.colorbar()
     ax[0].add_patch(ellipse_patch(e0.xc, e0.yc, e0.sma_pix, e0.ba, e0.theta_deg,
                                   edgecolor="cyan", linewidth=2))
     ax[0].add_patch(ellipse_patch(eph.xc, eph.yc, eph.sma_pix, eph.ba, eph.theta_deg,

hapy/scripts/run_analysis.py
@@ -27,7 +27,7 @@ parallel -j 8 python scripts/run_analysis.py --root {} --make-mask --galfit ::::
 
 One Step, Step 1/1:
 ```
-for d in cutouts/*/; do
+for d in cutouts/*/; 
   b=$(basename "$d")
   echo "${d}${b}"
 done | parallel -j 8 python scripts/run_analysis.py --root {} --make-mask --galfit
@@ -91,11 +91,11 @@ def init_cutout_logger(tag: str, cutdir: str | Path, level: str = "INFO",
     #cutdir = root.parent
 
     if log_dir is None:
-        log_dir = cutdir / "logs" / f"{tag}.analysis.log"
+        log_dir = cutdir / "logs" 
     else:
         log_dir = Path(log_dir)
     log_dir.mkdir(parents=True, exist_ok=True)
-    log_path = log_dir / f"{tag}.log"
+    log_path = log_dir /f"{tag}.analysis.log"
 
     logger = logging.getLogger(f"hapy.run_analysis.{tag}")
     logger.setLevel(getattr(logging, level.upper(), logging.INFO))
@@ -701,7 +701,7 @@ def main():
     prefix = tag
     results_path = cutdir / f"{tag}-results.ecsv"
 
-
+    print(f"DEBUG: cutdir={cutdir},tag={tag}, root={root}")
     # --- initialize logger
     logger, log_path = init_cutout_logger(
         tag=tag,
@@ -718,7 +718,9 @@ def main():
     if r_fits is None:
         raise FileNotFoundError(f"Could not find R-band FITS in: {cutdir}")
 
-    cs_fits = args.cs_fits or _pick_one(str(cutdir / f"{tag}*-CS-ZP.fits")) or _pick_one(str(cutdir / f"{tag}*-cs.fits")) 
+    cs_fits = args.cs_fits or _pick_one(str(cutdir / f"{tag}*-CS-ZP.fits")) or _pick_one(str(cutdir / f"{tag}*-cs.fits"))
+
+    # why are we looking for a mask when we are suppose to make one?
     mask_fits = args.mask_fits or _pick_one(str(cutdir / f"{tag}*-mask.fits"))
 
     
@@ -816,6 +818,7 @@ def main():
     row["FILTER_CORRECTION"] = params.get("filter_correction")
 
 
+    # TODO get this information from ZP ratio
     filter_ratio = params.get("filter_ratio", None)
     if filter_ratio is None:
         filter_ratio = np.nan
@@ -902,9 +905,9 @@ def main():
         t0 = time.perf_counter()
 
         # choose output mask name if not provided/found
-        mask_out = mask_fits or (root + "-mask.fits")
+        mask_out = mask_fits or (cutdir / f"{tag}-mask.fits")
 
-
+        print(f"DEBUG: mask_out={mask_out}")
 
 
 
@@ -944,7 +947,7 @@ def main():
             grow_iterations=int(args.grow_iterations),
         )
 
-        mask_out = mask_fits or (root + "-mask.fits")
+        #mask_out = mask_fits or (root + "-mask.fits")
         engine.write_mask(mask_out)
         mask_fits = mask_out
 
@@ -1130,7 +1133,7 @@ def main():
             theta_deg = phot_theta_deg
             )
         
-        outfile = Path(root).parent / f"{tag}-diagnostic.png"
+        outfile = cutdir / f"{tag}-diagnostic.png"
         plot_mask_ellipse_diagnostic(
             r_fits=str(r_fits),
             mask_fits=str(mask_fits),