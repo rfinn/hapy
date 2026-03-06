- fixed naming issue with *phot.fits files - changing snr calculation in radial profiles to snr_per_pixel to determine cutoff

hapy/ellipse/photometry.py
@@ -1346,9 +1346,9 @@ class EllipsePhotometry():
                 # compared with noise_per_pixel = sqrt(sky_uncertainty**2 + median_sb)
                 # snr_per_pixel = median_sb/noise
 
-                ave_sb = dF/dA
-                noise_per_pixel = np.sqrt(self.sky_noise**2 + ave_sb) # does not account properly for gain...
-                snr_per_pixel = ave_sb/noise_per_pixel
+                ave_sb_adu = dF/dA
+                noise_per_pixel_adu = np.sqrt((self.sky_noise*self.gain)**2 + self.gain*ave_sb_adu)/self.gain # does not account properly for gain...
+                snr_per_pixel = ave_sb_adu/noise_per_pixel_adu
                 
                 sigma_ann = self.get_noise_in_aper(dF, dA)
                 snr_ann =  dF / sigma_ann if (sigma_ann is not None and np.isfinite(sigma_ann) and sigma_ann > 0) else -np.inf