2b89a1e1b9684ae5443bec5189c6831e2a74f7a2
rfinn <rfinn@siena.edu>
2026-03-10 12:59:00 -0400

    - testing pipeline on draco, making slight improvements

--- hapy/scripts/get_cutouts.py
+++ hapy/scripts/get_cutouts.py
@@ -58,7 +58,7 @@ def main(args=None):
     help="Overwrite existing cutouts if they already exist"
     )
    
-    parser.add_argument('--maxcorrection',dest='maxcorrection', default=3, help='maximum filter correction for galaxies in FOV.  default is 3, so galaxies whose redshift falls where filter transmission < 33 percent will be skipped.')        
+    parser.add_argument('--maxcorrection', dest='maxcorrection', default=3., help='maximum filter correction for galaxies in FOV.  default is 3, so galaxies whose redshift falls where filter transmission < 33 percent will be skipped.')        
     #parser.add_argument('--oneimage',dest = 'oneimage',default=None, help='give full path to the r-band image name to run on just one image')
     
     args = parser.parse_args()
@@ -143,7 +143,7 @@ def main(args=None):
     if redshift is not None:
         myfilter = FilterTrace(image_set.h.filter, instrument=image_set.h.instrument)
         corrections = myfilter.get_trans_correction(redshift,outfile=None)
-        filter_keepflag = corrections < args.maxcorrection # this is a crazy big cut, but we can adjust with halphagui
+        filter_keepflag = corrections < float(args.maxcorrection) # this is a crazy big cut, but we can adjust with halphagui
         gcat.keepflag[gcat.keepflag] = filter_keepflag
         filter_corrections = corrections[filter_keepflag]
 
