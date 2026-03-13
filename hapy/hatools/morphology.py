9c02d87e05fa0b2001d27ed5b871e779814ca7ae
rfinn <rfinn@siena.edu>
2026-03-13 13:17:40 -0400

    - working on #22

--- hapy/hatools/morphology.py
+++ hapy/hatools/morphology.py
@@ -94,11 +94,10 @@ def run_statmorph_for_photometry(
     morph_r.print()
     morph2 = fig2 = None
     if image2 is not None:
-        morph2, fig2 = run_statmorph_single(
-            image=image2, segmap=segmap, gain=gain, mask=mask, psf=psf2, make_fig=make_fig)
+        morph2, fig2 = run_statmorph_single(image=image2, segmap=segmap, gain=gain, mask=mask, psf=psf2, make_fig=make_fig)
         print("STATMORPH RESULTS IMAGE2")
         morph2.print()
-        )
+        
 
     return MorphologyResult(morph_r=morph_r, morph_img2=morph2, fig_r=fig_r, fig_img2=fig2)
 
