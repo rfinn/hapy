7de143b5283487433c16dc35d9e55c8002a2d9a2
rfinn <rfinn@siena.edu>
2026-03-13 10:27:27 -0400

    - working on #21

--- hapy/scripts/run_analysis.py
+++ hapy/scripts/run_analysis.py
@@ -75,7 +75,8 @@ from hapy.galfittools.rungalfit import RunGalfit
 from hapy.imagetools.imutils import get_pixel_scale_from_filename
 from hapy.imagetools.plotting import plot_mask_ellipse_diagnostic
 from hapy.masktools.api import MaskEngine, EllipseParams
-from hapy.masktools.gaia import make_gaia_mask,  get_gaia_stars
+from hapy.masktools.gaia import make_gaia_mask,  get_gaia_stars, galaxy_overlaps_bright_star
+from hapy.masktools.maskops import distance_to_nearest_mask, largest_mask_region
 #from hapy.masktools.types import build_ell0_from_metadata
 from hapy.hatools.results import write_result_row_ecsv
 from hapy.geometry.adapters import pa_ccw_north_to_photutils_theta, photutils_theta_to_pa_ccw_north
