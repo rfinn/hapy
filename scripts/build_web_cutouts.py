3194baafe2a28645d21206248d3ad048af9f6568
rfinn <rfinn@siena.edu>
2026-03-16 13:09:03 -0400

    - working on #20

--- scripts/build_web_cutouts.py
+++ scripts/build_web_cutouts.py
@@ -428,6 +428,7 @@ class cutout_dir():
         self.results = tab[0] if len(tab) > 0 else None
         parent_dir = self.results['PARENT_RIMAGE'].replace('-r.fits','').replace('-R.fits','')
         self.parent_url = "../../coadds/{parent_dir}/"
+        print("DEBUG: parent_url = ",parent_url)
         #print("DEBUG: found results.ecsv file", self.results_file)
         #print("DEBUD: results colnames:")
         #print(self.results.colnames)
@@ -1394,7 +1395,7 @@ class build_html_cutout():
             pointing_str = f'<a href="../../coadds/{pointing}/{pointing}.html">{pointing}</a>'
         else:
             pointing_str = "--"
-
+        
         data = [
             get_result(self.cutout.results,"VFID", self.cutout.vfid),
             get_result(self.cutout.results,"GALNAME", self.cutout.gname),
