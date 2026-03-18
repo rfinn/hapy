1f3499c5998c1d227710b5bdf11e686a2379bd7e
rfinn <rfinn@siena.edu>
2026-03-18 08:43:55 -0400

    - working on #20

--- scripts/build_web_cutouts.py
+++ scripts/build_web_cutouts.py
@@ -1803,13 +1803,13 @@ class build_html_cutout():
             pdf_name = os.path.basename(self.cutout.sm_r_pdf)
             self.html.write(f'<p><b>r-band statmorph</b>: <a href="{pdf_name}">{pdf_name}</a></p>\n')
             #self.html.write(f'<iframe src="{pdf_name}" width="100%" "></iframe>\n')
-            self.html.write(f'<embed src="{pdf_name}" width="90%" height="700px"></embed>\n')
+            self.html.write(f'<embed src="{pdf_name}" width="90%" height="800px"></embed>\n')
 
         if self.cutout.sm_h_pdf is not None:
             pdf_name = os.path.basename(self.cutout.sm_h_pdf)
             self.html.write(f'<p><b>Halpha statmorph</b>: <a href="{pdf_name}">{pdf_name}</a></p>\n')
             #self.html.write(f'<iframe src="{pdf_name}" width="90%" height="700px" ></iframe>\n')
-            self.html.write(f'<embed src="{pdf_name}" width="90%" height="700px" ></embed\n')
+            self.html.write(f'<embed src="{pdf_name}" width="90%" height="800px" ></embed\n')
         
     # def write_statmorph_table(self):
     #     self.html.write('<h2>Statmorph Parameters</h2>\n')
