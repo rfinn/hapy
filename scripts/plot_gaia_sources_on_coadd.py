7163f342901a7d09cc1da4e75560ea023006fe74
rfinn <rfinn@siena.edu>
2026-04-17 12:22:10 -0400

    - working on #41 - gaia sources don't cover full field.  this code will create png files to overlay the gaia sources on the r-band image.

--- scripts/plot_gaia_sources_on_coadd.py
+++ scripts/plot_gaia_sources_on_coadd.py
@@ -88,9 +88,9 @@ def plot_gaia_overlay(image_fits, gaia_fits, outfile):
     ax.set_ylabel("y [pix]")
     ax.legend(loc="upper right", fontsize=8)
     fig.tight_layout()
-    fig.savefig(outfile, dpi=150)
+    fig.savefig(outfile, dpi=100)
     plt.close(fig)
-    print("saving figure:", time.time() - t0)
+    print("title, tighten, saving figure:", time.time() - t0)
     t0 = time.time()    
     print(f"Wrote: {outfile}")
 
