77ea8a16f4eca808089d758c16f432c927c41713
rfinn <rfinn@siena.edu>
2026-04-23 12:32:35 -0400

    - working on #41 - making a script to run mask using metadata.json for additional input.

--- hapy/scripts/run_maskgui_from_metadata.py
+++ hapy/scripts/run_maskgui_from_metadata.py
@@ -123,6 +123,7 @@ def main():
 
     if gaia_catalog is not None:
         cmd += ["--gaia-catalog", gaia_catalog]
+        # fixing names
         max_fwhm = max(meta["rimage_fwhm_psf_arcsec"], meta["himage_fwhm_psf_arcsec"])
         gaia_min_radius_arcsec = 4 * max_fwhm
         cmd += ["--gaia-min-radius", gaia_min_radius_arcsec]
