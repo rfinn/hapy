5615c256b48833686a93953f352f15bd87137f34
rfinn <rfinn@siena.edu>
2026-06-12 07:46:48 -0400

    - working on #54

--- hapy/scripts/get_cutouts.py
+++ hapy/scripts/get_cutouts.py
@@ -186,7 +186,8 @@ def main(args=None):
             #if "MOS" in args.rimage:
             #    print(f"Invalid regions in weight file for {args.rimage} - making cutout anyway")
             #else:
-            
+
+            # adding space around skipping message
             print(f"\nSkipping {galid[i]}: invalid cutout region ({status}); ra={gra[i]:.6f},dec={gdec[i]:.6f}\n")
             log.warning(
                 "SKIP_INVALID_REGION galid=%s ra=%.8f dec=%.8f size_arcsec=%.3f status=%s",
