6e643338d76a69929ef80301569878c5661b2113
rfinn <rfinn@siena.edu>
2026-06-21 12:38:37 -0400

    - working on new run - making mstar and sfr images

--- hapy/scripts/make_cs_gr.py
+++ hapy/scripts/make_cs_gr.py
@@ -807,7 +807,6 @@ if __name__ == '__main__':
         data_r_to_Ha,
         grow_radius=10,
         npixels=10,
-        weightimage=r_weight,
         nsigma=2.0,
         clip_sigma=3.0,
     )
@@ -817,7 +816,6 @@ if __name__ == '__main__':
         data_NB,
         grow_radius=10,
         npixels=10,
-        weightimage=r_weight,
         nsigma=2.0,
         clip_sigma=3.0,
     )
